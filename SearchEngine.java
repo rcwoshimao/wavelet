0586c9009ead72452fcce33e5458b7baea786bf6
rcwoshimao <mantoumiaoshen@gmail.com>
2023-04-12 16:22:02 -0700

    created search engine file

---
+++ SearchEngine.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String s = "";
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return "";
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    return String.format("Here is the string given: %s", parameters[1],s);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
